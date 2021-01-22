390e3631a0ea8580048cd5808f52dd2b794d4288
Nizar <nizar4790k@gmail.com>
2021-01-22 16:03:41 -0400

    navigation fixed

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,12 @@
 import React from 'react';
 
 const Navigation = () =>{
-<<<<<<< HEAD
-    return (
-        <nav style={{display:'flex',justifyContent:'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
-    );
-=======
     return(
         <nav  style={{display:'flex', justifyContent:'flex-end'}}>
         <p className='f3 link dim black uderline pa3 pointer'>Sign Out</p>
         </nav>
     );
     
->>>>>>> a863372dee1332ed287277af9e27278d2a8f32cb
 }
 
 export default Navigation;
